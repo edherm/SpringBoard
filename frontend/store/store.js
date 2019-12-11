66d7b047116f59581d9141518d6e90ad0a2ddc5b
Ed Herman <edward.herman.87@gmail.com>
2019-12-11 13:42:50 -0500

    Merge auth branch

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -2,10 +2,9 @@ import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import rootReducer from "../reducers/root_reducer";
-import { composeWithDevTools } from "redux-devtools-extension";
 
 const configureStore = (preloadedState = {}) => (
-  createStore(rootReducer, preloadedState, composeWithDevTools(applyMiddleware(thunk, logger)))
+  createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger))
 )
 
 export default configureStore;
