6b2202bc1bdb62941ac3e1faa2721144b2b30771
Ed Herman <edward.herman.87@gmail.com>
2020-01-22 12:27:06 -0500

    Implement and test message and messageErrors actions and reducers

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -3,10 +3,12 @@ import sessionErrorsReducer from './session_reducers/session_errors_reducer';
 import projectErrorsReducer from './projects_reducers/project_errors_reducer';
 import todoListsErrorsReducer from './todo_lists_reducers/todo_lists_errors_reducer';
 import todosErrorsReducer from './todos_reducers/todos_errors_reducer';
+import messagesErrorsReducer from "./messages_reducers/messages_errors_reducer";
 
 export default combineReducers({
   session: sessionErrorsReducer,
   projects: projectErrorsReducer,
   todoLists: todoListsErrorsReducer,
-  todos: todosErrorsReducer
+  todos: todosErrorsReducer,
+  messages: messagesErrorsReducer
 })
--- frontend/spring_board.jsx
+++ frontend/spring_board.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import configureStore from './store/store';
 import Root from './components/root';
-import { fetchMessages, fetchMessage, createMessage, updateMessage, deleteMessage } from "./util/messages_api_util";
+import { fetchMessages, fetchMessage, createMessage, updateMessage, deleteMessage } from "./actions/message_actions";
 
 
 document.addEventListener("DOMContentLoaded", () => {
