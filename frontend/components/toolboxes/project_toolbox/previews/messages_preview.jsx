25812e16050cdb56c073a39df43dc79067b0d4a1
Ed Herman <edward.herman.87@gmail.com>
2020-01-31 17:50:37 -0500

    Create default project previews

--- frontend/components/toolboxes/project_toolbox/previews/messages_preview.jsx
+++ frontend/components/toolboxes/project_toolbox/previews/messages_preview.jsx
@@ -10,24 +10,31 @@ class MessagesPreview extends React.Component {
   render () {
     const { messages, user } = this.props;
 
-    if (messages.length === 0) return null;
+    // if (messages.length === 0) return null;
 
     return (
       <li className="toolbox-item-li">
         <h1 className="toolbox-item-header">Message Board</h1>
         <div className="toolbox-item-body">
-          <ul className="preview message-ul">
-            {messages.map(message => {
-              return (
-                <MessageIndexItem 
-                  key={message.id} 
-                  page="preview" 
-                  message={message}
-                  user={user}
-                />
-              )
-            })}
-          </ul>
+          {messages.length === 0 ? (
+            <div className="empty-preview-wrapper">
+              <i class="fas fa-envelope-square"></i>
+              <p>Post announcements, pitch ideas, progress updates, etc.</p>
+            </div>
+          ) : (
+            <ul className="preview message-ul">
+              {messages.map(message => {
+                return (
+                  <MessageIndexItem 
+                    key={message.id} 
+                    page="preview" 
+                    message={message}
+                    user={user}
+                  />
+                )
+              })}
+            </ul>
+          )}
         </div>
       </li>
     );
--- frontend/components/toolboxes/project_toolbox/previews/todos_preview.jsx
+++ frontend/components/toolboxes/project_toolbox/previews/todos_preview.jsx
@@ -10,27 +10,37 @@ class TodosPreview extends React.Component {
   render () {
     const { todoLists, fetchTodos, projectId, todos } = this.props;
 
-    if (todoLists.length === 0) return null;
+    // if (todoLists.length === 0) return null;
 
     return (
       <li className="toolbox-item-li">
         <h1 className="toolbox-item-header" >To-dos</h1>
         <div className="toolbox-item-body" >
-          <ul className="todos-preview-ul" >
-            {todoLists.map(todoList => {
-              return (
-                <TodoList 
-                  key={todoList.id} 
-                  todoList={todoList}
-                  projectId={projectId}
-                  page="project"
-                  fetchTodos={fetchTodos}
-                  todos={todos} 
-                  updateTodo={null}
-                />
-              )
-            })}
-          </ul>
+          {todoLists.length === 0 ? (
+            <div className="empty-preview-wrapper">
+              <i className="fas fa-check-square"></i>
+              <p>
+                Make lists of work that needs to get done, assign items, set due 
+                dates, and discuss.
+              </p>
+            </div>
+          ) :(
+            <ul className="todos-preview-ul" >
+              {todoLists.map(todoList => {
+                return (
+                  <TodoList 
+                    key={todoList.id} 
+                    todoList={todoList}
+                    projectId={projectId}
+                    page="project"
+                    fetchTodos={fetchTodos}
+                    todos={todos} 
+                    updateTodo={null}
+                  />
+                )
+              })}
+            </ul>
+          )}
         </div>
       </li>
     )
