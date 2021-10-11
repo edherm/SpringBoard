b6b73a6556bb9b1987ab55e49c2b73307d03529b
Ed Herman <edward.herman.87@gmail.com>
2021-10-10 22:52:57 -0400

    Fix stale messages state

--- frontend/components/messages/messages_index_container.js
+++ frontend/components/messages/messages_index_container.js
@@ -1,24 +1,25 @@
-import { connect } from "react-redux";
-import MessagesIndex from "./messages_index";
-import { fetchMessages } from "../../actions/message_actions";
-import { fetchProject } from "../../actions/project_actions";
+import { connect } from 'react-redux';
+import MessagesIndex from './messages_index';
+import { fetchMessages } from '../../actions/message_actions';
+import { fetchProject } from '../../actions/project_actions';
 
 const msp = (state, ownProps) => {
+  const { projectId, userId } = ownProps.match.params;
   return {
-    messages: Object.values(state.entities.messages),
-    project: state.entities.projects[ownProps.match.params.projectId],
-    user: state.entities.users[ownProps.match.params.userId],
-    page: "index"
-  }
-}
+    messages: Object.values(state.entities.messages).filter((message) => {
+      return message.project_id === parseInt(projectId);
+    }),
+    project: state.entities.projects[projectId],
+    user: state.entities.users[userId],
+    page: 'index',
+  };
+};
 
-const mdp = dispatch => {
+const mdp = (dispatch) => {
   return {
-    fetchMessages: projectId => 
-      dispatch(fetchMessages(projectId)),
-    fetchProject: projectId => 
-      dispatch(fetchProject(projectId))
-  }
-}
+    fetchMessages: (projectId) => dispatch(fetchMessages(projectId)),
+    fetchProject: (projectId) => dispatch(fetchProject(projectId)),
+  };
+};
 
 export default connect(msp, mdp)(MessagesIndex);
